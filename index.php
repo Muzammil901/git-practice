3b57f416b6854b2fb422b21c08071cb45bbcde7f
Muhammad <muhammadmuzammil901@yahoo.com>
2022-05-26 21:04:49 +0500

    added html template

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Count App</title>
+</head>
+<body>
+	<div>
+		<form action="index.php" method="post">
+			Enter amount: <input type="number" name="number">
+			<input type="submit" name="submit" value="Enter">
+		</form>
+	</div>
+	<?php
+		if($_POST['submit']){
+			
+		}
+	?>
 
-echo "hello world";
-echo "software Engineer";
-
-?>
+</body>
+</html>
