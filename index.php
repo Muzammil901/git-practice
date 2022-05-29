982ddb9365bb43d14ca49c2301caa8afefee9299
Muhammad <muhammadmuzammil901@yahoo.com>
2022-05-29 21:57:15 +0500

    new lines of codes

--- index.php
+++ index.php
@@ -6,13 +6,21 @@
 <body>
 	<div>
 		<form action="index.php" method="post">
-			Enter amount: <input type="number" name="number">
+			Enter amount: <input type="number" name="amount">
 			<input type="submit" name="submit" value="Enter">
 		</form>
 	</div>
 	<?php
 		if($_POST['submit']){
-			
+			$listAmount = [];
+
+			$amount = $_POST['amount'];
+			array_push($listAmount, $amount);
+
+			for ($i=0; $i < count($listAmount); $i++) {
+				echo $listAmount[$i] . "<br>";
+			}
+
 		}
 	?>
 
